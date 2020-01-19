a310c3c9a93bf4e75a4dc5e63a7affafd8f728af
Deepak Chaudhary <chaudharydeepak08@gmail.com>
2020-01-19 00:40:38 -0600

    Updates

---
+++ precache-manifest.7269ccd020d0df4c3ce017311c35428a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ca631175bc8f07ceeaa408f1c356a4c",
+    "url": "/react-init-app/index.html"
+  },
+  {
+    "revision": "56dd57c20a333d44b85a",
+    "url": "/react-init-app/static/css/main.a4827b31.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/react-init-app/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-init-app/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56dd57c20a333d44b85a",
+    "url": "/react-init-app/static/js/main.d948cd11.chunk.js"
+  },
+  {
+    "revision": "f0bf786e45bd50a99c4f",
+    "url": "/react-init-app/static/js/runtime-main.0d45dc51.js"
+  }
+]);
